fa7382fe88b2f5e1296a4add19a538354f38d221
LamaElMalatawy <lamaelmalatawy@gmail.com>
2026-05-24 13:00:34 +0300

    Added permissions

--- src/EcommerceABP.Application.Contracts/Permissions/EcommerceABPPermissionDefinitionProvider.cs
+++ src/EcommerceABP.Application.Contracts/Permissions/EcommerceABPPermissionDefinitionProvider.cs
@@ -9,10 +9,20 @@ public class EcommerceABPPermissionDefinitionProvider : PermissionDefinitionProv
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(EcommerceABPPermissions.GroupName);
+        var myGroup = context.AddGroup(EcommerceABPPermissions.MainGroupName);
+        var productGroup = context.AddGroup(EcommerceABPPermissions.ProductGroupName, L("Products"));
+        var categoryGroup = context.AddGroup(EcommerceABPPermissions.CategoryGroupName, L("Categories"));
 
-        //Define your own permissions here. Example:
-        //myGroup.AddPermission(EcommerceABPPermissions.MyPermission1, L("Permission:MyPermission1"));
+        productGroup.AddPermission(EcommerceABPPermissions.CreateProductPermission, L("Permission:Products:CreateProduct"));
+        productGroup.AddPermission(EcommerceABPPermissions.UpdateProductPermission, L("Permission:Products:UpdateProduct"));
+        productGroup.AddPermission(EcommerceABPPermissions.DeleteProductPermission, L("Permission:Products:DeleteProduct"));
+        productGroup.AddPermission(EcommerceABPPermissions.ViewProductPermission, L("Permission:Products:ViewProduct"));
+        
+        categoryGroup.AddPermission(EcommerceABPPermissions.CreateCategoryPermission, L("Permission:Categories:CreateCategory"));
+        categoryGroup.AddPermission(EcommerceABPPermissions.UpdateCategoryPermission, L("Permission:Categories:UpdateCategory"));
+        categoryGroup.AddPermission(EcommerceABPPermissions.DeleteCategoryPermission, L("Permission:Categories:DeleteCategory"));
+        categoryGroup.AddPermission(EcommerceABPPermissions.ViewCategoryPermission, L("Permission:Categories:ViewCategory"));
+
     }
 
     private static LocalizableString L(string name)
--- src/EcommerceABP.Application.Contracts/Permissions/EcommerceABPPermissions.cs
+++ src/EcommerceABP.Application.Contracts/Permissions/EcommerceABPPermissions.cs
@@ -2,10 +2,21 @@ namespace EcommerceABP.Permissions;
 
 public static class EcommerceABPPermissions
 {
-    public const string GroupName = "EcommerceABP";
-
-
+    public const string MainGroupName = "EcommerceABP";
     
+    public const string ProductGroupName = MainGroupName + ".Products";
+    public const string CreateProductPermission = ProductGroupName + ".Create";
+    public const string UpdateProductPermission = ProductGroupName + ".Update";
+    public const string DeleteProductPermission = ProductGroupName + ".Delete";
+    public const string ViewProductPermission = ProductGroupName + ".View";
+
+    public const string CategoryGroupName = MainGroupName + ".Categories";
+    public const string CreateCategoryPermission = CategoryGroupName + ".Create";
+    public const string UpdateCategoryPermission = CategoryGroupName + ".Update";
+    public const string DeleteCategoryPermission = CategoryGroupName + ".Delete";
+    public const string ViewCategoryPermission = CategoryGroupName + ".View";
+
+
     //Add your own permission names. Example:
     //public const string MyPermission1 = GroupName + ".MyPermission1";
 }
